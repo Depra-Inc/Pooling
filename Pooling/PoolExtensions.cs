c257c882b89e07aa0c0fc142cfe947b2f3c7b543
n.melnikov <n.melnikov@depra.org>
2024-10-18 20:31:44 +0400

    Pool extension improved

--- Pooling/PoolExtensions.cs
+++ Pooling/PoolExtensions.cs
@@ -11,16 +11,21 @@ namespace Depra.Pooling
 		/// <summary>
 		/// Warm up the <see cref="IPool{TPooled}"/> by requesting and releasing <paramref name="count"/> objects.
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static void WarmUp<TPooled>(this IPool<TPooled> pool, int count)
-			where TPooled : IPooled
-		{
-			var collection = pool.RequestRange(count);
-			pool.ReleaseRange(collection);
-		}
+			where TPooled : IPooled =>
+			pool.ReleaseRange(pool.RequestRange(count));
+
+		/// <summary>
+		/// Warm up the <see cref="IPool"/> by requesting and releasing <paramref name="count"/> objects.
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void WarmUp(this IPool pool, int count) => pool.ReleaseRange(pool.RequestRange(count));
 
 		/// <summary>
 		/// Request a range of objects from the <see cref="IPool{TPooled}"/>.
 		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public static IEnumerable<TPooled> RequestRange<TPooled>(this IPool<TPooled> pool, int count)
 			where TPooled : IPooled
 		{
@@ -34,10 +39,26 @@ namespace Depra.Pooling
 		}
 
 		/// <summary>
-		/// Releases all objects in the list, the list should be cleared afterward.
+		/// Request a range of objects from the <see cref="IPool"/>.
 		/// </summary>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void ReleaseRange<TPooled>(this IPool<TPooled> pool, TPooled[] collection)
+		public static IEnumerable<IPooled> RequestRange(this IPool pool, int count)
+		{
+			var collection = new IPooled[count];
+			for (var index = 0; index < count; index++)
+			{
+				collection[index] = pool.RequestPooled();
+			}
+
+			return collection;
+		}
+
+		/// <summary>
+		/// Releases all objects in the list.
+		/// </summary>
+		/// <remarks>The list should be cleared afterward.</remarks>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void ReleaseRange<TPooled>(this IPool<TPooled> pool, IEnumerable<TPooled> collection)
 			where TPooled : IPooled
 		{
 			foreach (var item in collection)
@@ -47,15 +68,15 @@ namespace Depra.Pooling
 		}
 
 		/// <summary>
-		/// Releases all objects in the list, the list should be cleared afterward.
+		/// Releases all objects in the list.
 		/// </summary>
+		/// <remarks>The list should be cleared afterward.</remarks>
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
-		public static void ReleaseRange<TPooled>(this IPool<TPooled> pool, IEnumerable<TPooled> collection)
-			where TPooled : IPooled
+		public static void ReleaseRange(this IPool pool, IEnumerable<IPooled> collection)
 		{
 			foreach (var item in collection)
 			{
-				pool.Release(item);
+				pool.ReleasePooled(item);
 			}
 		}
 	}
